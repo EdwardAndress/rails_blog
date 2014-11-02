f114d4d350fd65dcf388104be45fa7d00ee25b23
edward andress <eddie_andress@hotmail.com>
2014-11-02 18:01:42 +0000

    added post model and completed form for post creation

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -24,6 +24,15 @@ describe 'posts' do
 			expect(page).to have_field "Cud of the Day"
 		end
 
+		it 'they can do so by filling in the appropriate form' do
+			visit '/posts'
+			click_link 'I have been ruminating on this for a while now and...'
+			fill_in 'Cud of the Day', with: 'Hey hey hey'
+			fill_in 'Tastes like...', with: 'Poo and wee'
+			click_button 'Burp'
+			expect(current_path).to eq '/posts'
+		end
+
 	end
 
 end
